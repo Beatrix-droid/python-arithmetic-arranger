c9a0ca2f88d42ae329d314bbf6a792afee4cb919
Beatrice <amilcare8@gmail.com>
2021-07-13 20:58:08 +0100

    final commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,72 +1,70 @@
-
-
 ''' A function created for arranging up to five addition and subtraction problems'''
 
 def arithmetic_arranger(maths, results=0):
 
 
-    '''Takes a list of maths problems and arranges them vertically. If the optional parameter "results" is inputted, the function alsO
-    displays the answers to the problems'''
+  '''Takes a list of maths problems and arranges them vertically. If the optional parameter "results" is inputted, the function alsO
+  displays the answers to the problems'''
 
-    if len(maths) > 5:
-        arranged_problems = "Error: Too many problems."
-        return arranged_problems
-    else:
-        toplines = ""
-        bottomlines = ""
-        lines = ""
-        answers = ""
-        operatorlist = []
-        for problem in maths:
-            problem_split = problem.split()
-            operatorlist.append(problem_split[1])
-            filteredmaths = list(filter(lambda z: z in ("-", "+"), operatorlist))
+  if len(maths) > 5:
+      arranged_problems = "Error: Too many problems."
+      return arranged_problems
+  else:
+    toplines = ""
+    bottomlines = ""
+    lines = ""
+    answers = "\n"
+    operatorlist = []
+    for problem in maths:
+        problem_split = problem.split()
+        operatorlist.append(problem_split[1])
+        filteredmaths = list(filter(lambda z: z in ("-", "+"), operatorlist))
         
-            if len(filteredmaths) != len(operatorlist):
-              arranged_problems = "Error: Operator must be '+' or '-'."
-              return arranged_problems
-            else:
-              operandlist = []
-              operandlist.append(problem_split[0])
-              operandlist.append(problem_split[2])
-              filteredmaths2 = list(filter(lambda z: not z.isdigit(),       operandlist))
-              if len(filteredmaths2) != 0:
-                arranged_problems = "Error: Numbers must only contain digits."
-                return arranged_problems
-              elif len(problem_split[0]) > 4 or len(problem_split[2]) > 4:
-                arranged_problems = "Error: Numbers cannot be more than four digits."
-                return arranged_problems
+        if len(filteredmaths) != len(operatorlist):
+          arranged_problems = "Error: Operator must be '+' or '-'."
+          return arranged_problems
+      
+        else:  
+          operandlist = []
+          operandlist.append(problem_split[0])
+          operandlist.append(problem_split[2])
+          filteredmaths2 = list(filter(lambda z: not z.isdigit(),       operandlist))
+          if len(filteredmaths2) != 0:
+            arranged_problems = "Error: Numbers must only contain digits."
+            return arranged_problems
+          elif len(problem_split[0]) > 4 or len(problem_split[2]) > 4:
+            arranged_problems = "Error: Numbers cannot be more than four digits."
+            return arranged_problems
 
-              else:
+          else:
                   
-                  length = max([len(problem_split[0]), len( problem_split[2])]) + 2
-                  toplines += (problem_split[0]).rjust(length)
-                  bottomlines += problem_split[1]+ (problem_split[2]).rjust(length-1)
+            length = max([len(problem_split[0]), len(problem_split[2])]) + 2
+                  
+            toplines += (problem_split[0]).rjust(length)      
+            bottomlines += problem_split[1]+ (problem_split[2]).rjust(length-1)
+            lines += length * "-"   
 
-                  if len(problem_split[0]) >= len(problem_split[2]):
-                        lines += (" " + (len(problem_split[0]) + 2) * "-").rjust(length-2)
-                  else:
-                        lines += (" " +  (len(problem_split[2]) + 2) * "-").rjust(length-2)
-
-                  if results:
-                        subtrctn_formatting = str(int(problem_split[0]) - int(problem_split[2])).rjust(length) + " "
-                        addtn_formatting = str(int(problem_split[0]) + int(problem_split[2])).rjust(length)+ "    "
-                        if problem_split[1] == "-":
-                            answers += subtrctn_formatting.format(length)
+            if problem != maths[-1]:
+              toplines += "    "
+              bottomlines += "    "
+              lines += "    "
+                    
+            if results:          
+              subtrctn_formatting = str(int(problem_split[0]) - int(problem_split[2])).rjust(length)
+              addtn_formatting = str(int(problem_split[0]) + int(problem_split[2])).rjust(length)
+              
+              if problem_split[1] == "-":
+                answers += subtrctn_formatting.format(length)
                               
-                        if problem_split[1] == "+":
-                                answers += addtn_formatting.format(length)
-                      
-                  else:
-                        answers += "\n"
-                  if problem != maths[-1]:
-                    toplines += "    "
-                    bottomlines += "    "
-                    lines += "    "
-                    answers += "    "
+              if problem_split[1] == "+":
+                answers += addtn_formatting.format(length)
 
-                          
-        arranged_problems = toplines +"\n" + bottomlines +"\n" + lines + "\n" + answers
+              if problem != maths[-1]:      
+                answers += "    "
+                  
+            else:
+              answers = ""
+                  
+    arranged_problems = toplines +"\n" + bottomlines +"\n" + lines +  answers
         
-        return arranged_problems
-            ##
+    return arranged_problems
